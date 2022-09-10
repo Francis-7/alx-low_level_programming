6bf70d9d7f54ebd719afa081bd30a1e347bfb59c
Francis Awoh <francisawoh54@gmail.com>
2022-09-10 01:38:17 -0700

    me commitinf

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	int i;
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+		return (0);
 }
