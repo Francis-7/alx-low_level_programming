682027cf407d79a09c9601e5610f48e3cb478721
Francis Awoh <francisawoh54@gmail.com>
2022-09-26 16:06:39 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,11 +7,12 @@
  * @n: no of bytes
  * Return: dest
  */
-char *_memcpy(char *dest, dest *s, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
+
 	return (dest);
 }
