604983085b2f3161885de64845e6bf620e62f34e
Francis Awoh <francisawoh54@gmail.com>
2022-09-26 22:27:08 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_strspn - Entry point
- *@s: input
- *@accept: input
- *Return: Always 0
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0
  */
-unsigned int _strspn(char *s, char accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
 	int r;
