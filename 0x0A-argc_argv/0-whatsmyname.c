b3a9ff4238cd5cc4e8035579f6930321d63c4d83
Francis Awoh <francisawoh54@gmail.com>
2022-09-30 04:40:00 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ *
+ * @argc: counts the number of arguements
+ * @argv: counts the string lenght
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char **argv)
 {
 	while (argc--)
