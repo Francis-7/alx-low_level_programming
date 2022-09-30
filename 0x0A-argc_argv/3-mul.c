d96706963bc3cbe30d216446641ee0e5bac85ce2
Francis Awoh <francisawoh54@gmail.com>
2022-09-30 05:46:35 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: count argument 
+ * @argv: string of the argument
+ *
+ * Return: on success zero on failure Error
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a *b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
