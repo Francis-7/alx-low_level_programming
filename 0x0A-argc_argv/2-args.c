e0abb56c78d25842133695d9164180bcfb9ccecc
Francis Awoh <francisawoh54@gmail.com>
2022-09-30 05:29:08 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: count of the argument
+ * @argv: the string of the argument
+ *
+ * Return: Always zero
+ */
+
+int main(int argc, char **argv)
+{
+        while(argc--)
+                printf("%s\n", *argv++);
+        exit(EXIT_SUCCESS);
+}
