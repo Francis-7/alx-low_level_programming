455201f4c924685915e62823f9f2847eab69431a
Francis Awoh <francisawoh54@gmail.com>
2022-10-10 14:33:00 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,7 +33,7 @@ char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
 
-	for (index = 0; src[index]; inex++)
+	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 
 	dest[index] = '\0';
