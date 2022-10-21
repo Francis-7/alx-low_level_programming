25d04006d60ae8ab6060d24eda38e912e5538f45
Francis Awoh <francisawoh54@gmail.com>
2022-10-20 21:27:52 -0700

    task 0, task 1, and task2

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+#include <stdio.h>>
+
+/**
+ * print_list - prints alll the elements of list_t list
+ * @h: the number of list_t
+ *
+ * Return: the number of nodes in h
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %c\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
